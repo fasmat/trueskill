2add560488a7989c0d00968fa9a781e518cf1d2a
Matthias Fasching <fasching.matthias@gmail.com>
2015-08-18 17:58:17 +0200

    Updated Team to incorporate recent changes.

--- team.go
+++ team.go
@@ -1,35 +1,32 @@
 package trueskill
 
-import "math"
-
 // Team is a composition of players that play together. The skill of a team
 // (µ and σ) is determined by the skills of the players that form the team.
 type Team struct {
 	players []Player
 }
 
+// NewEmptyTeam creates a team without any players. To add players use
+// AddPlayers.
+func NewEmptyTeam() Team {
+	return Team{
+		players: make([]Player, 0),
+	}
+}
+
 // NewTeam creates a team from a slice of players.
 func NewTeam(p []Player) (t Team) {
 	t.players = p
-	t.updateSkill()
 	return
 }
 
-// GetCurrentSkill returns the conservative skill estimate of the team. This
-// estimate is µ - 3σ. With 99.7% accuracy the actuall skill is higher than
-// this value.
-func (t *Team) GetCurrentSkill() float64 {
-	return t.mu - 3*t.sig
+// AddPlayers adds players to the team.
+func (t *Team) AddPlayers(p []Player) {
+	t.players = append(t.players, p...)
+	return
 }
 
-// updateSkill must be called everytime player composition changes or skills of
-// players change (e.g. after a game).
-func (t *Team) updateSkill() {
-	for _, p := range t.players {
-		t.mu += p.mu
-		t.sig += p.sig * p.sig
-	}
-
-	t.sig = math.Sqrt(t.sig)
-	return
+// GetPlayers returns the players the team is composed of.
+func (t *Team) GetPlayers() (p []Player) {
+	return t.players
 }
