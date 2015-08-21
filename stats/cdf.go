56ea87a147bbbc8f19645f64f6a573ebdd66589d
Matthias Fasching <fasching.matthias@gmail.com>
2015-08-21 17:25:46 +0200

    Removed duplicate funcitons

--- stats/cdf.go
+++ stats/cdf.go
@@ -2,28 +2,6 @@ package stats
 
 import "math"
 
-// GetDrawMargin returns the draw margin epsilon given a draw probability.
-//
-//  pDraw is the probability of a draw of equally skilled players [0..1]
-//  beta specifies how much skill difference is defined to give a player 80%
-//    chance of winning over the lower skilled player (see DefBeta).
-//  nPlayers defines how many players against each other, independent of team
-//    composition (e.g. 2 for chess, 3 for skat or 22 for soccer)
-func GetDrawMargin(pDraw, beta float64, nPlayers uint) float64 {
-	return InverseCDF((pDraw+1)/2) * math.Sqrt(float64(nPlayers)) * beta
-}
-
-// GetDrawProbability returns the draw probability given a draw margin.
-//
-//  epsilon defines the draw margin of the game
-//  beta specifies how much skill difference is defined to give a player 80%
-//    chance of winning over the lower skilled player (see DefBeta).
-//  nPlayers defines how many players against each other, independent of team
-//    composition (e.g. 2 for chess, 3 for skat or 22 for soccer)
-func GetDrawProbability(epsilon, beta float64, nPlayers uint) float64 {
-	return 2 * NormalCDF(epsilon/math.Sqrt(float64(nPlayers)*beta)-1)
-}
-
 // NormalCDF calculates the Cumulative Distribution Function for the
 // standardized normal distribution
 func NormalCDF(x float64) float64 {
