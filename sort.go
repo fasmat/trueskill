2fafa55b542260663708534eaf47beccb25d037a
Matthias Fasching <fasching.matthias@gmail.com>
2015-09-02 11:24:58 +0200

    Added sorting function for players.
    Sorting functions are now also public.

--- sort.go
+++ sort.go
@@ -2,20 +2,42 @@ package trueskill
 
 import "sort"
 
-// ByRank implements sort.Interface for []Team based on rankings
-type ByRank struct {
+type playersByRank struct {
+	data []Player
+	rank []int
+}
+
+func (t playersByRank) Len() int { return len(t.data) }
+func (t playersByRank) Swap(i, j int) {
+	t.data[i], t.data[j] = t.data[j], t.data[i]
+	t.rank[i], t.rank[j] = t.rank[j], t.rank[i]
+}
+func (t playersByRank) Less(i, j int) bool { return t.rank[i] < t.rank[j] }
+
+// SortPlayersByRank sorts players by their rankings. The returned values
+// have the same length as the provided arguments but are sorted from
+// lowest (best) to highest (worst) rank.
+func SortPlayersByRank(data []Player, rank []int) ([]Player, []int) {
+	sort.Sort(playersByRank{data, rank})
+	return data, rank
+}
+
+type teamsByRank struct {
 	data []Team
 	rank []int
 }
 
-func (t ByRank) Len() int { return len(t.data) }
-func (t ByRank) Swap(i, j int) {
+func (t teamsByRank) Len() int { return len(t.data) }
+func (t teamsByRank) Swap(i, j int) {
 	t.data[i], t.data[j] = t.data[j], t.data[i]
 	t.rank[i], t.rank[j] = t.rank[j], t.rank[i]
 }
-func (t ByRank) Less(i, j int) bool { return t.rank[i] < t.rank[j] }
+func (t teamsByRank) Less(i, j int) bool { return t.rank[i] < t.rank[j] }
 
-func sortTeamsByRank(data []Team, rank []int) ([]Team, []int) {
-	sort.Sort(ByRank{data, rank})
+// SortTeamsByRank sorts teams by their rankings. The returned values
+// have the same length as the provided arguments but are sorted from
+// lowest (best) to highest (worst) rank.
+func SortTeamsByRank(data []Team, rank []int) ([]Team, []int) {
+	sort.Sort(teamsByRank{data, rank})
 	return data, rank
 }
--- sort_test.go
+++ sort_test.go
@@ -2,16 +2,64 @@ package trueskill
 
 import "testing"
 
-func TestSortByRank(t *testing.T) {
-	dataIn := []Team{NewEmptyTeam(), NewEmptyTeam(), NewEmptyTeam(),
-		NewEmptyTeam()}
-	rankIn := []int{4, 3, 1, 1}
+func TestSortPlayersByRank(t *testing.T) {
+	players := generatePlayers(5)
 
-	dataOut := []Team{NewEmptyTeam(), NewEmptyTeam(), NewEmptyTeam(),
-		NewEmptyTeam()}
-	rankOut := []int{1, 1, 3, 4}
+	dataIn := []Player{
+		players[0],
+		players[4],
+		players[3],
+		players[1],
+		players[2],
+	}
 
-	data, rank := sortTeamsByRank(dataIn, rankIn)
+	rankIn := []int{0, 4, 3, 1, 2}
+
+	data, rank := SortPlayersByRank(dataIn, rankIn)
+
+	if len(data) != len(players) {
+		t.Error("Lost data values during sorting. Expected",
+			len(players), "got", len(data))
+	}
+
+	if len(rank) != len(rankIn) {
+		t.Error("Lost rank values during sorting. Expected",
+			len(rankIn), "got", len(rank))
+	}
+
+	for i, v := range data {
+		if v.GetID() != i {
+			t.Error("Element", i, "of players is not the same as expected")
+		}
+
+		if rank[i] != i {
+			t.Error("Element", i, "of rank is not the same as expected")
+		}
+	}
+}
+
+func TestSortTeamsByRank(t *testing.T) {
+
+	players := generatePlayers(5)
+	dataIn := []Team{
+		NewTeam([]Player{players[4]}),
+		NewTeam([]Player{players[3]}),
+		NewTeam([]Player{players[2]}),
+		NewTeam([]Player{players[1]}),
+		NewTeam([]Player{players[0]}),
+	}
+	rankIn := []int{5, 4, 3, 1, 1}
+
+	dataOut := []Team{
+		NewTeam([]Player{players[1]}),
+		NewTeam([]Player{players[0]}),
+		NewTeam([]Player{players[2]}),
+		NewTeam([]Player{players[3]}),
+		NewTeam([]Player{players[4]}),
+	}
+	rankOut := []int{1, 1, 3, 4, 5}
+
+	data, rank := SortTeamsByRank(dataIn, rankIn)
 
 	if len(data) != len(dataOut) {
 		t.Error("Lost data values during sorting. Expected",
@@ -23,9 +71,24 @@ func TestSortByRank(t *testing.T) {
 			len(rankOut), "got", len(rank))
 	}
 
+	for i, v := range data {
+		if v.String() != dataOut[i].String() {
+			t.Error("Element", i, "of teams is not the same as expected")
+		}
+	}
+
 	for i, v := range rank {
 		if v != rankOut[i] {
 			t.Error("Element", i, "of rank is not the same as expected")
 		}
 	}
+}
+
+func generatePlayers(n int) []Player {
+	p := make([]Player, n)
+
+	for i := 0; i < n; i++ {
+		p[i] = NewDefaultPlayer(i)
+	}
+	return p
 }
